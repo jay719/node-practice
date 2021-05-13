60f1689a4f55fb2fc548179473b63858c4c29207
jay719 <javariab17@gmail.com>
2021-05-12 23:06:12 -0600

    appendFileSync

--- first-practice/app.js
+++ first-practice/app.js
@@ -1,3 +1,4 @@
 const fs = require ('fs')
-fs.writeFileSync('notes.txt', 'My name is javaria') 
+fs.writeFileSync('notes.txt', 'I live in Colorado') 
 
+fs.appendFileSync('notes.txt', ' My name is javaria')
